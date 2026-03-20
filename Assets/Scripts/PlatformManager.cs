dcc8936158d84c4302636117f658d1afb44b64b5
cacaguaton <157182702+cacaguaton@users.noreply.github.com>
2026-03-19 19:58:28 -0600

    MagnetFixeado

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -12,14 +12,20 @@ public class PlatformManager : MonoBehaviour
     [SerializeField]
     private int initialPlatforms = 5;
     [SerializeField]
-    private float speed = 5f;
+    private float minSpeed = 5f;
+    [SerializeField]
+    private float maxSeed = 12f;
+    [SerializeField]
+    private float acceleration = 0.1f;
     [SerializeField]
     private UnityEvent<Platform> onPlatformPased;
     private bool isRunning = true;
     private GameObject lastPlatform;
     private int platformsInstantiated = 0;
+    private float speed;
     public void StartGame()
     {
+        speed = minSpeed;
         lastPlatform = null;
         platformsInstantiated = 0;
         InitializePlatforms();
@@ -70,6 +76,7 @@ public class PlatformManager : MonoBehaviour
         if (isRunning)
         {
             transform.Translate(Vector3.back * speed * Time.deltaTime);
+            speed = Mathf.Min(speed + acceleration * Time.deltaTime, maxSeed);
         }
     }
     public void StopPlatforms()
